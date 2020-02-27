807fd5e692fc50f0b4bd85a4e3ca86678cb90668
Izabela Draga <izabdr@gmail.com>
2020-02-27 18:19:02 +0100

    first run

--- src/main/java/jaworskipr/jaworski/controller/HomeController.java
+++ src/main/java/jaworskipr/jaworski/controller/HomeController.java
@@ -1,7 +1,14 @@
 package jaworskipr.jaworski.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path = "/jaworskipr.com")
 public class HomeController {
+    @GetMapping(path = "/")
+    public String homePageShow(Model model) {
+        return "index"; }
 }
